94e77bd33629b6ba63d48b5627c625565dd32bf5
Matias Bassil <matias.bassil@ulb.ac.be>
2019-03-01 02:31:09 +0000

    Update main.cpp

--- ami test/client/srcs/main.cpp
+++ ami test/client/srcs/main.cpp
@@ -1,7 +1,11 @@
-#include "Serveur.hpp"
+#include "client.hpp"
 
-
-int main(){
-    Serveur serveur(5555);
+int main(int argc, char *argv[]){
+    if(argc > 1)
+    {	Client* client = new Client();
+    	client->Run(argv[1]);
+    }
+    else
+    	std::cout << "name parameter required " << std::endl;
     return 0;
 }
