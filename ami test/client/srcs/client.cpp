b91dd9323176732cf568f4b539af9425138d1fdc
Matias Bassil <matias.bassil@ulb.ac.be>
2019-03-01 02:05:27 +0000

    Update client.cpp

--- ami test/client/srcs/client.cpp
+++ ami test/client/srcs/client.cpp
@@ -1,312 +1,103 @@
-#include "client.hpp"
+#include "Serveur.hpp"
 
+Serveur::Serveur(){}
 
-Client::Client(){
-	this->_request = new Request(); //pq dynamiquement ?
-	//firstWindow();
+Serveur::Serveur(short unsigned int port) : _port(port) {
+    this->setup();
 }
 
-//pour aller plus vite dans les tests
-void Client::Run(std::string n )
-{
-	std::cout<< "sending:" << n << std::endl;
-	_request->login(n,"ignore");
-	firstWindow();
-}
+void Serveur::setup(){
+    this->_address.sin_family = AF_INET;
+    this->_address.sin_port = htons(this->_port);
+    this->_address.sin_addr.s_addr = INADDR_ANY;
+    this->_addrlen = sizeof(this->_address);
 
+    if ((this->_serv_sock = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        std::cout << "Error: " << strerror(errno) << std::endl;
+        this->sShutdown();
+    } else {
+        std::cout << "Socket created !" << std::endl;
+    }
 
+    if (bind(this->_serv_sock, reinterpret_cast<struct sockaddr*>(&this->_address), this->_addrlen) < 0){
+        std::cout << "Error on bind: " << strerror(errno) << std::endl;
+        this->sShutdown();
+    } else {
+        std::cout << "Bind successfull !" << std::endl;
+    }
 
+    if (listen(this->_serv_sock, 3) < 0){
+        std::cout << "Error: " << strerror(errno) << std::endl;
+        this->sShutdown();
+    }
 
-
-Client::~Client(){
-	std::cout << "Destructor"  << std::endl;
-	//delete this->_requests; ???????
-}
-
-void Client::connectionError(){
-	std::cout << "Connection with server lost : " << strerror(errno) << std::endl;
-	delete this; // :(
-}
-
-void Client::startingGame(bool playerTurn){
-	this->_gameStart = true;
-	//this->_myTurn = playerTurn;
-}
-
-void Client::opponentMov(int coord1, int coord2, bool lose){
-	;;//to do
+    this->mainLoop();
 }
 
 
 
-void Client::firstWindow(){
-	
-	friendsWindow();
-}
 
+std::vector<User*> onlineUsers; 
 
 /*
-void Client::firstWindow(){
-	bool log = false;
-	bool exit = false;
-	char answer;
-	while(! log && !exit){
-		while ( answer != '1' && answer != '2' ) {
-			std::cout << "Write 1 for login, 2 for register or 3 for exit:" << std::endl;
-			std::cin >> answer;
-			this->myFlush();
-		}
-		if (answer == '1')
-			log=logInWindow();
-		else if (answer == '2')
-			log=registerWindow();
-		else
-			exit = true;
-	}
-	if (! exit){
-		menuWindow();
-	}
-}
+on update la bdd seulement quand on ajoute/supprimme ami ou a chaque deconnexions ?
+todo rendre onlineUsers thread safe?
+
 */
 
-
-void Client::listOnlineFriends()
-{
-	_request->listOnlineFriends();
-}
-
-
-//use mutex pour voir liste d'amis
-void Client::friendsWindow()
-{
-	unsigned int res = 0;
-	
-	while(res != 7)
-	{
-		std::cout << "Que désirez vous faire?: " << std::endl;
-		std::cout << "1) Ajouter un ami " << std::endl;
-		std::cout << "2) Lister tous les amis connectés " << std::endl;
-		std::cout << "3) Supprimer un ami " << std::endl;
-		std::cout << "4) Consulter mes demandes d'amis/de parties " << std::endl;
-		std::cout << "5) Proposer à un ami de faire une partie " << std::endl;
-		std::cout << "6) Chat avec des amis " << std::endl;
-		std::cout << "7) Retourner au menu principal " << std::endl;
-
-		std::cin >> res;
-		while(res == 0 || res > 7)
-		{	
-			std::cout << "Choix invalide veuillez réessayer:" << std::endl;
-			std::cin >> res;
-		}
-		
-
-		switch (res)
-		{
-			case 1:
-			
-				std::string friendName;
-				std::cout<< "Veuillez entrer le nom de l'ami à ajouter: ";
-				std::cin >> friendName;
-				if(!_request->addFriend(friendName))
-					std::cout << friendName << " n'existe pas " << std::endl;
-				
-				break;
-
-			case 2:
-				_request->listOnlineFriends();
-
-			case 3:
-				_request->listOnlineFriends();
-
-			case 4:
-			
-				std::cout << "écrivez \"exit\" pour revenir au menu" <<std::endl;
-				std::string tmp;
-				while(tmp != "exit")
-					std::cin >> tmp;
-				break;
-			case 5:
-				break;
-
-			case 6:
-				break;
-			case 7:
-				break;
-
-
-			else
-			{
-				std::cout << "Choix invalide veuillez réessayer:" << std::endl;
-			}
-		}
-	}
-}
-
-
-
-
-
-bool Client::registerWindow(){
-    std::string username;
-    std::string password;
-    std::string password2;
-    std::string email;
-    std::regex regEmail("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])");
-    while(true){
-        std::cout << "Write your new username: ";
-        std::cin >> username;
-        myFlush();
-        std::cout << "Write your email: ";
-        std::cin >> email;
-        myFlush();
-        password = getpass("New password (will not be shown): ");
-        password2 = getpass("New password (again): ");
-        if (password != password2){
-            std::cout << "Password does not match!" << std::endl;
-            continue;
-        }
-        if (! std::regex_match(email.begin(), email.end(), regEmail)){
-			std::cout << "Invalid email." << std::endl;
-			continue;
-        }
-		if (this->_request->letsRegister(username,password,email))
-			return true;
-		else{
-			char answer;
-			std::cout << "Write 1 for continue or 2 for exit: ";
-			std::cin >> answer;
-			while (answer != '1' && answer != '2'){
-				std::cout << "Please write 1 for continue or 2 for exit: ";
-				std::cin >> answer;
-			}
-			if (answer != '2')
-				return false;
-		}
-    }
-}
-
-bool Client::logInWindow(){
-	std::string username;
-    std::string password;
-    while(true){
-        std::cout << "Write your username :";
-        std::cin >> username;
-        this->myFlush();
-        password = getpass("Password (password will not be shown) :");
-        if(this->_request->login(username,password)){
-			std::cout << "You are now logged in !" << std::endl;
-			return true;
-		}
-		else{
-			char answer;
-			std::cout << "Invalid username or password." << std::endl;
-			std::cout << "Write 1 for continue or 2 for exit: ";
-			std::cin >> answer;
-			while (answer != '1' && answer != '2'){
-				std::cout << "Please write 1 for continue or 2 for exit: ";
-				std::cin >> answer;
-			}
-			if (answer != '2')
-				return false;
-		}
-    }
-}
-
-void Client::menuWindow(){
-	char answer;
-	bool waitForGame = false;
+void Serveur::mainLoop(){
+    std::thread cmdThread(&Serveur::handleCommand, *this);
+    int tmpClient;
+ 
     while (true){
-		if (this->_gameStart){
-			gameWindow();
-			waitForGame = false;
-		}
-        std::cout << "Enter 1 for exit, 2 for chat";
-		if (!waitForGame)
-			std::cout <<", 3 for game";
-		std::cout << ": " << std::endl;
-        std::cin >> answer;
-		myFlush();
-        if (answer == '1'){
+        if ((tmpClient = accept(this->_serv_sock, reinterpret_cast<struct sockaddr*>(&this->_address), reinterpret_cast<socklen_t*>(&this->_addrlen))) >= 0){
+            std::cout << "Nouvelle connexion et le socket est : " << tmpClient << std::endl;
+            if (tmpClient != -1){
+                if (static_cast<size_t>(tmpClient) > this->_clients.size()){
+                     this->_clients.resize(static_cast<size_t>(tmpClient));       
+                    std::cout << "resize" << std::endl;
+                }
+                this->_clients.at(static_cast<unsigned long int>(tmpClient-1)) = tmpClient;
+                User* tmpUser = new User(tmpClient); // <------ new important pour polymorphisme! - Quentin
+                
+                /*
+                j'ai juste laissé ca pour un test au cas ou je veux que chaque utilisateur soit ami avec tout le monde
+                for(int i = 0; i < onlineUsers.size(); i++)
+                {
+                    //query bdd if(tmpUser friends with onlineUsers[i]) on les ajoute en amis
+                    onlineUsers[i]->addFriendToList(tmpUser);
+                    tmpUser->addFriendToList(onlineUsers[i]);
+                    load bdd ici ?
+                }*/
+                //load les amis de la bdd ici ?
+                onlineUsers.push_back(tmpUser);
+            }
+        }
+    }
+}
+
+
+
+
+void* Serveur::handleCommand(){
+    std::string command;
+    while (true) {
+        getline(std::cin, command);
+        if (command == "shutdown"){
+            this->sShutdown();
             break;
         }
-        else if (answer == '2'){
-            ;;// To Do
-        }
-        else if (answer == '3' && !waitForGame){
-            waitForGame = selectGameModeWindow();
-        }
     }
+    return NULL;
 }
 
-bool Client::selectGameModeWindow(){
-	char answer;
-    std::cout << "Enter 1 for classic, 2 for Dark, 3 for Trappist, 4 for Anti or 5 for return to the menu: " << std::endl;
-    std::cin >> answer;
-	myFlush();
-    while (answer != '1' && answer != '2' && answer != '3' && answer != '4' && answer != '5'){
-        std::cout << "Please, enter 1 for classic, 2 for Dark, 3 for Trappist, 4 for Anti or 5 for return to the menu: " << std::endl;
-        std::cin >> answer;
-		myFlush();
-    }
-	if (answer == '5')
-		return false;
-	else{
-		this->_request->findMatch(static_cast<int>(answer));
-		;;//set bord
-		return true;
-	}
-}
 
-void Client::gameWindow(){
-//	//this->_lose = false;
-//	std::string answer;
-//    std::vector<int> listMov;
-//	int coord1;
-//	int coord2;
-//    while (true){
-//        std::cout << "Enter 1 for surrend, 2 for chat, 3 for checkmov";
-//        if (! listMov.empty())
-//                std::cout << ", 4 for mov";
-//        std::cout << std::endl;
-//        std::cin >> answer;
-//		myFlush();
-//        if (strcmp(answer.c_str(),"1")){
-//			this->_request->surrend();
-//            break;
-//        }
-//        else if (strcmp(answer.c_str(),"2")){
-//            ;;
-//        }
-//        else if (strcmp(answer.c_str(),"3") && this->_myTurn){ //getMov
-//			std::cout << "Enter the letter of the colomne and the number of the line (ex: 'B3'): ";
-//			std::cin >> answer;
-//			myFlush();
-//			//if ( check with regex ){
-//				//coord1 = convertStringToCoord(answer);
-//				//listMov = this->_request->getMov(coord);
-//			//}
-//        }
-//        else if(!listMov.empty() && strcmp(answer.c_str(),"4") && this->_myTurn){ //mov
-//			coord2 = -1;
-//			while (std::find(listMov.begin(), listMov.end(), coord2) != listMov.end()){
-//				std::cout << "Enter the letter of the colomne and the number of the line (ex: 'B3'): ";
-//				std::cin >> answer;
-//				myFlush();
-//				//coord2 = convertStringToCoord(answer);
-//			}
-//			this->_myTurn = false;
-//			//this->_bord->updateMov();
-//			this->_request->mov(coord1,coord2);
-//        }
-//		else{
-//			std::cout << "Please wait your turn." << std::endl;
-//		}
-//    }
-}
-
-void Client::myFlush(){
-    if (std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+void Serveur::sShutdown(){
+    shutdown(this->_serv_sock, SHUT_RDWR);
+    for (unsigned long int i = 0; i < this->_clients.size(); i++){
+        close(this->_clients.at(i));
     }
+    close(this->_serv_sock);
+    std::cout << "Server is now offline." << std::endl;
+    exit(EXIT_SUCCESS);
 }
