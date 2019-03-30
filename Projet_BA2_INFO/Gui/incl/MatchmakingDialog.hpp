aab86ee2b3456440ecb4d8ca6c4464bc108d2037
alaamiri <laamiri-achraf@hotmail.com>
2019-03-30 22:49:16 +0100

    matchmaking presque fini GUI

---
+++ Projet_BA2_INFO/Gui/incl/MatchmakingDialog.hpp
@@ -0,0 +1,28 @@
+#ifndef MATCHMAKINGDIALOG_HPP
+#define MATCHMAKINGDIALOG_HPP
+
+#include <QDialog>
+#include <QLabel>
+#include <QtWidgets>
+#include <iostream>
+
+
+class MatchmakingDialog : public QDialog {
+
+public:
+    MatchmakingDialog(QWidget* parent = nullptr);
+    void fondOpponent(std::string opponent);
+    QPushButton* getOkButton();
+    QPushButton* getCancelButton();
+private:
+    void init_window();
+    void init_layout();
+
+    QGridLayout* _gbox;
+    QLabel* _label;
+    QPushButton* _okButton;
+    QPushButton* _cancelButton;
+
+};
+
+#endif // MATCHMAKINGDIALOG_HPP
