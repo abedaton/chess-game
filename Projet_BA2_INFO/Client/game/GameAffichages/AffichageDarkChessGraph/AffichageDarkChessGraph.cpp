7b4d33d8681930c7dafa5674b8de18a30a24db2f
Quentin Demonceau <quentin-demonceau@hotmail.com>
2019-04-01 23:07:30 +0200

    purge master

--- Projet_BA2_INFO/Client/game/GameAffichages/AffichageDarkChessGraph/AffichageDarkChessGraph.cpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "AffichageDarkChessGraph.hpp"
-
-AffichageDarkChessGraph::AffichageDarkChessGraph() : AffichageDarkChessGraph() {} //*< Constructor
--- Projet_BA2_INFO/Serveur/game/GameAffichages/AffichageGraph/AffichageGraph.cpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "AffichageGraph.hpp"
-
-AffichageGraph::AffichageGraph() : Affichage() {} //*< Constructor
